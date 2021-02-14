41907f0e34a8b67cfa1445a279d864a849ddbbf9
H. Muller <hxmuller@gmail.com>
2021-02-14 15:06:31 -0500

    refactor: Comment code for future me

--- script.js
+++ script.js
@@ -13,15 +13,19 @@ const form = document.querySelector('#sizePicker');
 form.addEventListener('submit', function(event) {
   let rows = parseInt(heightInput.value, 10);
   let cols = parseInt(widthInput.value, 10);
+  // Grid disappears immediately unless default action prevented
   event.preventDefault();
+  // Reset the canvas
   clearTable(table);
   colorInput.value = "#000000"
+  // Create new canvas and listeners
   makeGrid(table, rows, cols);
   addCellListeners(table);
 });
 
 /**
  * @description Creates the design canvas
+ * @param {table} table - The table to process
  * @param {number} rows - The height in rows of the canvas
  * @param (number} cols - The width in columns of each row of canvas
  */
@@ -36,7 +40,7 @@ function makeGrid(table, rows, cols) {
 
 /**
  * @description Add listeners to design canvas cells
- * @param {table} - The table to operate on
+ * @param {table} table - The table to process
  */
 function addCellListeners(table) {
   let cells = table.querySelectorAll('td');
@@ -49,6 +53,7 @@ function addCellListeners(table) {
 
 /**
  * @description Removes all child elements of table
+ * @param {table} table - The table to process
  */
 function clearTable(table) {
   while (table.firstChild) {
