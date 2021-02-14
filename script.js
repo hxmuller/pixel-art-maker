60c4765d44b5fad41f3570b1e7c69a4894d98f76
H. Muller <hxmuller@gmail.com>
2021-02-13 20:40:59 -0500

    Add working makeGrid() function

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // Select color input
 // Select size input
+let height = 16;
+let width = 16;
+
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+/**
+ * @description Creates the design canvas
+ * @param {number} rows - The height in rows of the canvas
+ * @param (number} cols - The width in columns of each row of canvas
+ */
+function makeGrid(rows, cols) {
+  let newTable = document.querySelector('#pixelCanvas');
+  for (let row = 0; row < rows; row++) {
+    let newRow = newTable.insertRow(row);
+    for (let col = 0; col < cols; col++) {
+      let newCell = newRow.insertCell(col);
+    }
+  }
 }
+
+makeGrid(height, width);
